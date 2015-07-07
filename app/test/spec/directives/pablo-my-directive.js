8ac4cac61d3e47d6e7199ff7436c8f5b5b5b6294
Thomas Steglich <info@thomas-steglich.de>
2015-07-07 23:02:55 +0200

    test for directive completed

--- app/test/spec/directives/pablo-my-directive.js
+++ app/test/spec/directives/pablo-my-directive.js
@@ -1,5 +1,7 @@
 describe('Directive: myDirective', function() {
 
+	'use strict';
+
 	beforeEach(module('myDirectiveModule'));
 	beforeEach(module('templates'));
 
@@ -10,12 +12,35 @@ describe('Directive: myDirective', function() {
 		scope = $rootScope.$new();
 		element = angular.element('<my-directive something="thing"></my-directive>');
 		element = $compile(element)(scope);
-		scope.thing = {name: 'My thing'};
+		scope.thing = {
+			name: 'My thing'
+		};
 		scope.$digest();
 	}));
 
-	it ('should render something', function() {
+	it('should render something', function() {
 		var h1 = element.find('h1');
 		expect(h1.text()).toBe('My thing');
+	});
+
+	it('should update the rendered text when scope changes', function() {
+		scope.thing.name = 'My new thing';
+		scope.$digest();
+		var h1 = element.find('h1');
+		expect(h1.text()).toBe('My new thing');
+	});
+
+	describe('Directive controller', function() {
+
+		var controller;
+		beforeEach(function() {
+			controller = element.controller('myDirective');
+		});
+
+		it('should do something', function() {
+			expect(controller.doSomething).toBeDefined();
+			controller.doSomething();
+			expect(controller.something.name).toBe('Do something');
+		});
 	});
 });
